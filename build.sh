7e94c72d01ab5092ccde6125c8f2be05f2038fd9
Alessandro <dorigo.alessandro@gmail.com>
2019-12-05 11:00:21 +0100

    updating the gnl

--- build.sh
+++
@@ -1,2 +0,0 @@
-gcc get_next_line.c get_next_line_util.c test/test.c -o get_next_line.out -Wall -Wextra -Werror
-printf "Compiled!\n"
--- get_next_line.c
+++ get_next_line.c
@@ -6,95 +6,70 @@
 /*   By: adorigo <adorigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:54:16 by adorigo           #+#    #+#             */
-/*   Updated: 2019/10/24 14:25:44 by adorigo          ###   ########.fr       */
+/*   Updated: 2019/11/22 13:35:35 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-void	*ft_calloc(int count, int size);
-char	*ft_strjoin(char *s1,char *s2);
-char	*ft_strstr(const char *haystack,const char *needle);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-int		ft_memtreat(char *ptr, const void *src, size_t n, char flag);
+#include "libft.h"
 
-int				clean_mem(char **buf)
+static char		*ft_strnew(void)
 {
-	free(*buf);
-	*buf = NULL;
-	return (ERROR);
+	char *ret;
+
+	if (!(ret = (char *)malloc(sizeof(char))))
+		return (0);
+	ret[0] = '\0';
+	return (ret);
 }
 
-static int		read_fd(char **buf, int fd, int *pos)
+static int		extract(char **line, char **cache, int idx)
 {
-	int     ret;
-	char    *ptr;
-	char    buffer[BUFF_SIZE + 1];
+	char	*tmp;
+	int		ret;
 
-	while (1)
+	if (idx >= 0)
 	{
-		if ((ret = read(fd, buffer, BUFF_SIZE)) == ERROR)
-			return (ERROR);
-		buffer[ret] = 0;
-		if (ret > 0 && (ptr = ft_strjoin(buf[fd], buffer)) != NULL)
-		{
-			free(buf[fd]);
-			buf[fd] = ptr;
-		}
-		if ((ptr = ft_strstr(buf[fd], "\n")) != NULL)
-		{
-			*pos = ptr - buf[fd];
-			return (SUCCESS);
-		} else
-			*pos = ft_memtreat(buf[fd], NULL, 0, 0);
-		if (ret == 0)
-			return (ENDFILE);
-	}
-}
-
-static char		*make_newline(char *lst_ptr, int ret, int pos)
-{
-	char	*new_line;
-	
-	if (ret == ENDFILE)
-	{
-		if (!(new_line = (char *)ft_calloc(pos + 1, sizeof(char))))
-			return (NULL);
-		ft_memtreat(new_line, lst_ptr, pos, 1);
-		ft_memmove(lst_ptr, lst_ptr + pos, 1);
+		if (!(*line = ft_substr(*cache, 0, idx)))
+			return (free_cache(cache, -1));
+		if (!(tmp = ft_substr(*cache, idx + 1, ft_strlen(*cache) - idx - 1)))
+			return (free_cache(cache, -1));
+		ret = 1;
 	}
 	else
 	{
-		if (!(new_line = (char *)ft_calloc(pos + 1, sizeof(char))))
-			return (NULL);
-		ft_memtreat(new_line, lst_ptr, pos, 1);
-		ft_memmove(lst_ptr, lst_ptr + pos + 1, ft_memtreat(lst_ptr + pos, NULL, 0, 0));	
+		if (!(*line = ft_substr(*cache, 0, ft_strlen(*cache))))
+			return (free_cache(cache, -1));
+		tmp = 0;
+		ret = 0;
 	}
-	return (new_line);
+	free_cache(cache, 0);
+	*cache = tmp;
+	return (ret);
 }
 
 int				get_next_line(int fd, char **line)
 {
-	static char		*buffer[FDS_MAX];
-	int             pos;
-	int             ret;
+	ssize_t		r_size;
+	char		buff[BUFFER_SIZE + 1];
+	static char	*cache;
+	char		*tmp;
 
-	if (fd < 0 || !line || BUFF_SIZE < 0)
-		return (ERROR);
-	if (buffer[fd] == NULL)
-		buffer[fd] = (char *)(ft_calloc(1, sizeof(char)));
-	if ((ret = read_fd(buffer, fd, &pos)) < 0)
-		return (clean_mem(&(buffer[fd])));
-		if (!(*line = make_newline(buffer[fd], ret, pos)))
-		return (clean_mem(&(buffer[fd])));
-	if (*(*line) != 0 || ret != ENDFILE)
-		return (SUCCESS);
-	else 
+	if (fd < 0 || !line || fd > OPEN_MAX || BUFFER_SIZE <= 0)
+		return (free_cache(&cache, -1));
+	while ((r_size = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
-		if (!(*line = (char *)ft_calloc(pos + 1, sizeof(char))))
-			return (clean_mem(&(buffer[fd])));
-		*(*line) = 0;
-		free(buffer[fd]);
-		buffer[fd] = NULL;
-		return (ENDFILE);
+		buff[r_size] = '\0';
+		if (!(tmp = ft_strnjoin(cache, buff, r_size)))
+			return (free_cache(&cache, -1));
+		free_cache(&cache, 0);
+		cache = tmp;
+		if (ft_strnbr(cache) != -1)
+			break ;
 	}
+	if (r_size < 0)
+		return (free_cache(&cache, -1));
+	if (r_size == 0 && (!cache || *cache == '\0')
+		&& (*line = ft_strnew()))
+		return (free_cache(&cache, 0));
+	return (extract(line, &cache, ft_strnbr(cache)));
 }
